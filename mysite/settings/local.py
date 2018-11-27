487c449b94e698d6fae6ab3254eb713c2a5e1485
anYesh Shrestha <sthacruz@gmail.com>
2018-11-27 22:49:07 +0545

    fixed urls

--- mysite/settings/local.py
+++ mysite/settings/local.py
@@ -134,7 +134,10 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static-cdn-local')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
